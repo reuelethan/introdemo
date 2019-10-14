b80ce833a2838b77045bc9b32a67cdacbecf0ab1
Mac User <User@Macs-iMac-5.local>
2019-10-14 11:22:33 -0400

    intial commit

---
+++ script.js
@@ -0,0 +1,19 @@
+let button = document.querySelector("button");
+let body = document.querySelector("body");
+
+button.addEventListener("click", test);
+
+function test() {
+    body.style.backgroundColor = "green";
+
+}
+$("div").click(function(){
+    $("div").addClass("bounce");
+    setTimeout(function(){
+      $("div").removeClass("bounce")
+      
+    }, 2000)
+    
+  })
+  
+  
